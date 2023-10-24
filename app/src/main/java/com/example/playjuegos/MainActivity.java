e2e1fed429144aa17ecabd421a6d969d8bf19fad
carolmt <carolinamaldonado12@hotmail.es>
2023-10-24 16:54:30 +0200

    parte 2

--- app/src/main/java/com/example/playjuegos/MainActivity.java
+++ app/src/main/java/com/example/playjuegos/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +25,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_buscar) {
+            Toast.makeText(this, "Búsqueda", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
     public void lanzarNewPlayer() {
         Intent i = new Intent(this, NewPlayer.class);
         startActivity(i);
